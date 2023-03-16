052f3dc29b1ea5706f9d1e9c6f0df3678e17dfd9
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-03-16 09:15:10 -0400

    Create function for gt theme

--- R/wp_pageviews_taylor_swift.R
+++ R/wp_pageviews_taylor_swift.R
@@ -81,6 +81,55 @@ data_tb <- data_full %>%
 
 
 # Visualize data ----------------------------------------------------------
+theme_spotify <- function(gt_object, background_colour = "#2d2d2d", ...) {
+  stopifnot("'gt_object' must be a 'gt_tbl'." = "gt_tbl" %in% class(gt_object))
+  
+  gt_object %>%
+    gt::tab_options(
+      heading.align = "left",
+      heading.border.bottom.style = "none",
+      table.background.color = background_colour,
+      table.font.color.light = "white",
+      table.border.top.style = "none",
+      table.border.bottom.color = background_colour,
+      table.border.left.color = background_colour,
+      table.border.right.color = background_colour,
+      table_body.border.top.style = "none",
+      table_body.border.bottom.color = background_colour,
+      column_labels.border.top.style = "none",
+      column_labels.background.color = background_colour,
+      column_labels.border.bottom.width = 3,
+      column_labels.border.bottom.color = background_colour,
+      data_row.padding = gt::px(7),
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        color = "white",
+        font = gt::google_font("Source Sans Pro"),
+        transform = "uppercase"
+      ),
+      locations = gt::cells_column_labels(tidyselect::everything())
+    ) %>%
+    gt::tab_style(
+      style = gt::cell_text(
+        font = gt::google_font("Libre Franklin"),
+        weight = 800,
+        size = "xx-large"
+      ),
+      locations = gt::cells_title(groups = "title")
+    ) %>%
+    gt::tab_style(style = list(
+      gt::cell_text(font = gt::google_font("Source Sans Pro"),
+                    weight = 400),
+      gt::cell_borders(color = background_colour)
+    ),
+    locations = gt::cells_body()) %>%
+    # Add padding
+    gt::opt_horizontal_padding(scale = 3) %>%
+    gt::opt_vertical_padding(scale = 1.5) %>%
+    gt::opt_table_outline(style = "none", width = gt::px(0))
+  
+}
 
 gt_export <- data_tb %>%
   gt() %>%
@@ -103,53 +152,11 @@ gt_export <- data_tb %>%
   fmt_number(views, sep_mark = ",", decimals = 0) %>%
   fmt_number(length, sep_mark = ",", decimals = 0) %>%
   # Customized version of gtExtras::gt_theme_dark()
-  tab_options(
-    heading.align = "left",
-    heading.border.bottom.style = "none",
-    table.background.color = "#2d2d2d",
-    table.font.color.light = "white",
-    table.border.top.style = "none",
-    table.border.bottom.color = "#2d2d2d",
-    table.border.left.color = "#2d2d2d",
-    table.border.right.color = "#2d2d2d",
-    table_body.border.top.style = "none",
-    table_body.border.bottom.color = "#2d2d2d",
-    column_labels.border.top.style = "none",
-    column_labels.background.color = "#2d2d2d",
-    column_labels.border.bottom.width = 3,
-    column_labels.border.bottom.color = "#2d2d2d",
-    data_row.padding = px(7)
-  ) %>%
-  tab_style(
-    style = cell_text(
-      color = "white",
-      font = google_font("Source Sans Pro"),
-      transform = "uppercase"
-    ),
-    locations = cells_column_labels(everything())
-  ) %>%
-  tab_style(
-    style = cell_text(
-      font = google_font("Libre Franklin"),
-      weight = 800,
-      size = "xx-large"
-    ),
-    locations = cells_title(groups = "title")
-  ) %>%
-  tab_style(style = list(
-    cell_text(font = google_font("Source Sans Pro"),
-              weight = 400),
-    cell_borders(color = "#2d2d2d")
-  ),
-  locations = cells_body()) %>%
+  theme_spotify() %>% 
   gt_fa_column(like,
                prefer_type = "solid",
                palette = "lightgreen",
-               align = "center") %>%
-  # Add padding
-  opt_horizontal_padding(scale = 3) %>%
-  opt_vertical_padding(scale = 1.5) %>%
-  opt_table_outline(., style = "none", width = px(0))
+               align = "center")
 
 # View graph
 gt_export
