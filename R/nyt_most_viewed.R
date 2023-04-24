a30655ef7a5e3a6145c2835f7f928f65123179c3
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-24 12:42:04 -0400

    Add data wrangling

--- R/nyt_most_viewed.R
+++ R/nyt_most_viewed.R
@@ -38,11 +38,77 @@ library("wikirest")
 
 # Get data ----------------------------------------------------------------
 
+# Set parameters
+yesterday <- Sys.Date()-1
+
+year <- format(yesterday, "%Y")
+month <- format(yesterday, "%m")
+day <- format(yesterday, "%d")
+
+
 data <- get_most_viewed_per_country(
   country = "US",
   access = "all-access",
-  year = "2023",
-  month = "04",
-  day = "24",
+  year = year,
+  month = month,
+  day = day,
   tidy = TRUE
 )
+
+
+# Wrangle and tidy data ---------------------------------------------------
+
+exclude <- c("Main Page", "Special:Search", "Wikipédia:Accueil principal", 
+             "Wikipedia:Featured pictures", "Spécial:Recherche", 
+             "Portal:Current events", "Wikipedia:首页", "Wiktionary:Main Page",
+             "Wikidata:Copyright")
+
+# Keep top articles
+data_table <- data %>% 
+  # Remove pages that aren't articles
+  dplyr::filter(!article %in% exclude) %>% 
+  # Create new rank column based on articles
+  select(!rank) %>% 
+  tibble::rowid_to_column("rank") %>% 
+  # Keep top 10
+  filter(rank <= 15) %>% 
+  # Create language column
+  separate(project, c("language", "project"), "\\.")
+
+
+# Visualise data ----------------------------------------------------------
+
+month <- format(yesterday, "%B")
+
+subtitle <- paste0("Most viewed articles in the US on ", month, " ", day, ",", " ", year, ".")
+
+caption_1 <- paste0("Source: Wikimedia REST API.")
+caption_2 <- "Code: github.com/judith-bourque"
+
+views_min <- min(df_table$views_ceil)
+views_max <- max(df_table$views_ceil)
+
+
+# Create graph
+gt_export <- df_table %>% 
+  select(c(rank, article, language, views_ceil)) %>% 
+  gt() %>% 
+  cols_label(
+    language = "Lang",
+    views_ceil = "Views ceiling"
+  ) %>% 
+  # Add space in numbers
+  fmt_number(views_ceil, sep_mark = " ", decimals = 0) %>% 
+  tab_header(
+    title = md("**What are Canadians reading on Wikipedia?**"),
+    subtitle = subtitle
+  ) %>% 
+  gt_color_rows(views_ceil, palette = "ggsci::red_material", domain = c(views_min, views_max)) %>% 
+  tab_source_note(caption_1) %>% 
+  tab_source_note(caption_2) %>% 
+  gt_theme_538()
+
+# View graph
+gtsave(gt_export, "graph/graph.png")
+
+knitr::include_graphics('graph/wp_pageviews_top_in_canada.png')
