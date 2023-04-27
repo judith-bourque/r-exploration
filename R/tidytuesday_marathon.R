4680ea11644be9c5584c659bb7d7dd599b410464
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-27 19:28:20 -0400

    Add border around point

--- R/tidytuesday_marathon.R
+++ R/tidytuesday_marathon.R
@@ -14,7 +14,7 @@ winners_tidy <- janitor::clean_names(winners)
 
 # Analyse data ------------------------------------------------------------
 
-wins_by_nationality <- winners_tidy %>% 
+wins_by_nationality <- winners_tidy %>%
   count(nationality)
 
 # Compute the centroid as the mean longitude and lattitude
@@ -23,30 +23,41 @@ region_lab_data <- world_maps %>%
   group_by(region) %>%
   summarise(longitude = mean(long), latitude = mean(lat))
 
-point_data <- inner_join(wins_by_nationality, region_lab_data, by = c("nationality" = "region")) %>% 
-  arrange(desc(n)) %>% 
-  mutate(rank = row_number(),
-         colour = case_when(rank == "1" ~ "1st place",
-                            rank == "2" ~ "2nd place",
-                            rank == "3" ~ "3rd place",
-                            .default = "not on podium"
-                            ))
+point_data <-
+  inner_join(wins_by_nationality,
+             region_lab_data,
+             by = c("nationality" = "region")) %>%
+  arrange(desc(n)) %>%
+  mutate(
+    rank = row_number(),
+    colour = case_when(
+      rank == "1" ~ "1st place",
+      rank == "2" ~ "2nd place",
+      rank == "3" ~ "3rd place",
+      .default = "not on podium"
+    )
+  )
 
 # Visualise data ----------------------------------------------------------
 
 colours <-
-  c("1st place" = "gold",
+  c(
+    "1st place" = "gold",
     "2nd place" = "black",
     "3rd place" = "brown",
-    "not on podium" = "white")
+    "not on podium" = "white"
+  )
 
 graph <-
   ggplot() +
-  borders(
-    fill = "#f7dc99",
-    colour = "white"
+  borders(fill = "#f7dc99",
+          colour = "white") +
+  geom_point(
+    aes(longitude, latitude, size = n, fill = colour, stroke = 3),
+    data = point_data,
+    pch = 21,
+    alpha = 0.5
   ) +
-  geom_point(aes(longitude, latitude, size = n, fill = colour), data = point_data, pch=21, alpha = 0.5) +
   theme_void() +
   scale_fill_manual(values = colours) +
   labs(title = "Title",
