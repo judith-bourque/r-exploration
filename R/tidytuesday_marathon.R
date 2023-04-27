9eda38895ecc2c3deeb1e016993708af98f71bdd
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-27 16:30:10 -0400

    Tidy data

--- R/tidytuesday_marathon.R
+++ R/tidytuesday_marathon.R
@@ -4,3 +4,17 @@ library("tidyverse")
 tuesdata <- tidytuesdayR::tt_load('2023-04-25')
 
 winners <- tuesdata$winners
+
+
+# Tidy data ---------------------------------------------------------------
+
+winners_tidy <- janitor::clean_names(winners)
+
+# Analyse data ------------------------------------------------------------
+
+graph_data <- winners %>% 
+  count(nationality)
+
+# Visualise data ----------------------------------------------------------
+
+#ggplot(graph_data, aes())
