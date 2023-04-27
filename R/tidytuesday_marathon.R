58c83913304ce21193e3af8cb626079bb2a127d0
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-04-27 16:34:07 -0400

    Get map labels

--- R/tidytuesday_marathon.R
+++ R/tidytuesday_marathon.R
@@ -5,6 +5,8 @@ tuesdata <- tidytuesdayR::tt_load('2023-04-25')
 
 winners <- tuesdata$winners
 
+# Get map data
+world_maps <- map_data("world")
 
 # Tidy data ---------------------------------------------------------------
 
@@ -15,6 +17,12 @@ winners_tidy <- janitor::clean_names(winners)
 graph_data <- winners %>% 
   count(nationality)
 
+# Compute the centroid as the mean longitude and lattitude
+# Used as label coordinate for country's names
+region_lab_data <- world_maps %>%
+  filter(region %in% winners_tidy$nationality) %>%
+  group_by(region) %>%
+  summarise(longitude = mean(long), latitude = mean(lat))
+
 # Visualise data ----------------------------------------------------------
 
-#ggplot(graph_data, aes())
